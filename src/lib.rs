57ee5c9689389ab9d4033a61e527f5f4856da0c2
bit-aloo <sshourya17@gmail.com>
2025-10-24 12:27:07 +0530

    add memtable abstraction

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+#![allow(warnings)]
+pub mod iterators;
+pub mod key;
+pub mod lsm_storage;
+pub mod mem_table;
+pub mod wal;
---
+++ src/wal.rs
@@ -0,0 +1,33 @@
+use std::{fs::File, path::Path, sync::Arc};
+
+use bytes::Bytes;
+use crossbeam_skiplist::SkipMap;
+use parking_lot::Mutex;
+
+use crate::key::KeySlice;
+
+pub struct Wal {
+    file: Arc<Mutex<File>>,
+}
+
+impl Wal {
+    pub fn create(path: impl AsRef<Path>) -> Result<Self> {
+        unimplemented!()
+    }
+
+    pub fn recover(path: impl AsRef<Path>, skiplist: &SkipMap<Bytes, Bytes>) -> Result<Self> {
+        unimplemented!()
+    }
+
+    pub fn put(&self, key: &[u8], value: &[u8]) -> Result<()> {
+        unimplemented!()
+    }
+
+    pub fn put_batch(&self, data: &[(KeySlice, &[u8])]) -> Result<()> {
+        unimplemented!()
+    }
+
+    pub fn sync(&self) -> Result<()> {
+        unimplemented!()
+    }
+}
