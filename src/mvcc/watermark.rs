395bccadd299df45ac83e1d8bbbdb5b115df4c3c
bit-aloo <sshourya17@gmail.com>
2025-11-25 21:28:44 +0530

    add watermark

--- src/mvcc/watermark.rs
+++ src/mvcc/watermark.rs
@@ -17,9 +17,17 @@ impl Watermark {
         }
     }
 
-    pub fn add_reader(&mut self, ts: u64) {}
+    pub fn add_reader(&mut self, ts: u64) {
+        *self.readers.entry(ts).or_default() += 1;
+    }
 
-    pub fn remove_reader(&mut self, ts: u64) {}
+    pub fn remove_reader(&mut self, ts: u64) {
+        let cnt = self.readers.get_mut(&ts).unwrap();
+        *cnt -= 1;
+        if *cnt == 0 {
+            self.readers.remove(&ts);
+        }
+    }
 
     pub fn num_retained_snapshots(&self) -> usize {
         self.readers.len()
